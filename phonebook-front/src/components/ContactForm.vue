ae1c1e6b4099a66696ab94b7a70456f9fed24dc3
ericjbs <ericjuliob@gmail.com>
2023-04-10 23:38:56 -0300

    Add a functional contact list with front search

--- phonebook-front/src/components/ContactForm.vue
+++ phonebook-front/src/components/ContactForm.vue
@@ -1,21 +1,19 @@
 <template>
-    <div>
-        
-    </div>
+    <div></div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            contacts: [],
-            firstName: '',
-            lastName: '',
-            phoneNumber: ''
-        }
-    },
-    methods: {
-        
-    }
-}
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
+
+<style scoped>
+/* Estilos do Bootstrap */
+@import url("https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css");
+/* Estilos do Font Awesome */
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css");
+</style>
--- phonebook-front/src/components/ContactList.vue
+++ phonebook-front/src/components/ContactList.vue
@@ -1,56 +1,99 @@
 <template>
-  <div class="container">
-    <h1>Contacts</h1>
-    <hr />
-    <div class="list-group">
-      <p v-if="contacts.length <= 0">No contacts...</p>
-      <div
-        v-else
-        class="list-group-item"
-        v-for="(contact, index) in allContacts"
-        v-bind:key="index"
-      >
-        <span class="fullName"
-          ><strong>{{ contact.firstName }} {{ contact.LastName }}</strong></span
-        >
-        <div>
-          <a href="#" title="Delete" v-on:click.prevent="removeContact(index)"
-            >Delete</a
-          >
-        </div>
+  <div class="container mt-5">
+    <h1 class="text-center mb-4">Contacts List</h1>
+    <div class="row">
+      <div class="col-sm-6 col-md-6 col-lg-6 offset-md-3">
+        <form class="form-inline mb-4" @submit.prevent="searchContacts">
+          <input
+            v-model="searchTerm"
+            type="text"
+            class="form-control col-sm-12 col-md-12 col-lg-12"
+            placeholder="Search for contact by last name"
+          />
+        </form>
       </div>
     </div>
-    <hr />
+    <div class="row">
+      <div class="col-sm-6 col-md-6 col-lg-6 offset-md-3">
+        <ul class="list-group">
+          <li
+            v-for="contact in filteredContacts"
+            :key="contact.id"
+            class="list-group-item d-flex justify-content-between align-items-center"
+          >
+            <div>
+              <h5 class="mb-1">{{ contact.name }}</h5>
+              <p class="mb-1">{{ contact.phone }}</p>
+            </div>
+            <div>
+              <button
+                @click="deleteContact(contact.id)"
+                class="btn btn-danger btn-md"
+              >
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   data() {
     return {
-      contacts: [],
-      firstName: "",
-      lastName: "",
-      phoneNumber: "",
+      searchTerm: "",
+      filteredContacts: [],
+      contacts: [
+        {
+          id: 1,
+          name: "Contact Name 1",
+          email: "contact1@example.com",
+          phone: "123456789",
+          otherInformation: "Other information 1",
+        },
+        {
+          id: 2,
+          name: "Contact Name 2",
+          email: "contact2@example.com",
+          phone: "987654321",
+          otherInformation: "Other information 2",
+        },
+        // Add more contacts here
+      ],
     };
   },
   methods: {
-    addContact(contact) {
-      this.contacts.push(contact);
+    searchContacts() {
+      const searchTerm = this.searchTerm.toLowerCase();
+
+      // Filter contacts based on search term
+      this.filteredContacts = this.contacts.filter((contact) => {
+        return (
+          contact.name.toLowerCase().includes(searchTerm) ||
+          contact.email.toLowerCase().includes(searchTerm)
+        );
+      });
     },
-    removeContact(index) {
-        this.contacts.splice(index);
+    clearSearch() {
+      this.searchTerm = ""; // Clear search term
+      this.filteredContacts = this.contacts; // Update filtered contacts list to show all contacts
+    },
+    deleteContact(contactId) {
+      this.contacts.splice(contactId);
     },
   },
-  computed: {
-    allContacts() {
-      return this.contacts.map((contact) => ({
-        ...contact,
-        firstName: contact.firstName.trim(),
-        lastName: contact.lastName.trim(),
-        phoneNumber: contact.phoneNumber.trim(),
-      }));
+  watch: {
+    searchTerm() {
+      this.searchContacts();
     },
   },
 };
 </script>
+<style scoped>
+/* Bootstrap Styles */
+@import url("https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css");
+/* Font Awesome Styles */
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css");
+</style>
