6c486eec4016dcdcf6250257646eb08c534f4220
eric-meli <eric.silva@mercadolivre.com>
2023-04-12 10:11:51 -0300

    Remove unused component

--- phonebook-front/src/App.vue
+++ phonebook-front/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
-    <ContactForm v-on:add-todo="addContact"></ContactForm>
     <ContactList />
   </div>
 </template>
 
 <script>
 import ContactList from "./components/ContactList.vue";
-import ContactForm from "./components/ContactForm.vue";
 
 export default {
   name: "App",
   components: {
-    ContactForm,
-    ContactList,
+    ContactList
   },
 };
 </script>
